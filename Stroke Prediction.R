83d813fc502effb0f9ed483d90e2ad5cdcdbf7bb
Craig Moat <craigmoat@Craigs-MacBook-Air-2.local>
2025-12-14 11:06:22 +0000

    Stroke Prediction Project

---
+++ Stroke Prediction.R
@@ -0,0 +1,8 @@
+library(dplyr)
+library(janitor)
+library(ggplot2)
+library(tibble)
+# read healthcare_dataset_stroke_csv and assign it to the ogject 'sds' for ease 
+# of use. Converts N/A to missing entries 
+sds <- read.csv("healthcare_dataset_stroke_data.csv", header = TRUE,
+                na.strings = c("N/A", ""))
